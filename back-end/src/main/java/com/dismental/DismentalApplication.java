fa6018aca5b2cb05a99c64029e60db86db743b97
rajatpandey441 <rajatpandey441@gmail.com>
2023-05-09 17:10:43 -0400

    first commit

---
+++ back-end/src/main/java/com/dismental/DismentalApplication.java
@@ -0,0 +1,13 @@
+package com.dismental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DismentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DismentalApplication.class, args);
+	}
+
+}
